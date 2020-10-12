39082126459332fc1edef915570137637ddaa80b
Arda Demir <52732986+ardademr1@users.noreply.github.com>
2020-10-12 13:08:38 +0300

    King

---
+++ loginController.ts
@@ -0,0 +1,9 @@
+
+module.exports.login_get = function(req,res){
+    // passport error mesajı üretir.
+    console.log(req.session.flash.error);
+    res.render("login.ejs");
+}
+module.exports.login_post = (req,res)=>{
+    
+}
