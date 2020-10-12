83904bab6f3d90c6db8ccfe55b2cd9bda42b23ea
Arda Demir <52732986+ardademr1@users.noreply.github.com>
2020-10-12 13:09:24 +0300

    Delete loginController.ts

--- loginController.ts
+++
@@ -1,9 +0,0 @@
-
-module.exports.login_get = function(req,res){
-    // passport error mesajı üretir.
-    console.log(req.session.flash.error);
-    res.render("login.ejs");
-}
-module.exports.login_post = (req,res)=>{
-    
-}
