6f881a75cd2f2e64a48fa48679bc58e0eed878d9
Arda Demir <52732986+ardademr1@users.noreply.github.com>
2020-10-12 13:10:53 +0300

    gg

--- controller/loginController.ts
+++ controller/loginController.ts
@@ -1,9 +1,9 @@
 
-module.exports.index = function(req,res){
-    res.render("login");
+module.exports.login_get = function(req,res){
+    // passport error mesajı üretir.
+    console.log(req.session.flash.error);
+    res.render("login.ejs");
 }
 module.exports.login_post = (req,res)=>{
-    const {email, password}=req.body;
-    console.log(email,password);
-    res.render("login");
+    
 }
