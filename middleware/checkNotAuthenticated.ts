611b0a234a1bd4c36c3cb713e0acd5c19c181ce6
Arda Demir <52732986+ardademr1@users.noreply.github.com>
2020-10-21 16:19:04 +0300

    Add files via upload

---
+++ middleware/checkNotAuthenticated.ts
@@ -0,0 +1,10 @@
+export {};
+const passport = require("passport");
+
+module.exports = function checkAuthenticated(req, res, next) {
+    console.log(req.isAuthenticated());
+    if (req.isAuthenticated()) {
+      return res.redirect("/users/dashboard");
+    }
+    next();
+  }
